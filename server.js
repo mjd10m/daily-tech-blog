277c5f12bd71685cdbdf9103942a2ebd652d4a14
mjd10m <mjd10m@outlook.com>
2022-11-05 21:36:18 -0400

    models added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const routes = require('./controllers');
+/*const routes = require('./controllers');*/
 const sequelize = require('./config/connection');
 const path = require('path')
 const exphbs = require('express-handlebars')
-const helpers = require('./utils/helpers')
-const hbs = exphbs.create({helpers})
+/*const helpers = require('./utils/helpers')*/
+const hbs = exphbs.create({})
 const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const sess = {
@@ -27,9 +27,9 @@ app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
 // turn on routes
-app.use(routes);
+/*app.use(routes);*/
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
